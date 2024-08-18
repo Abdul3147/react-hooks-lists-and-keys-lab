701c521a2aab6ade66fd9ba7d16bf152d6f4074c
Abdul3147 <abdallahussein228@gmail.com>
2024-08-18 17:38:56 +0300

    done

--- src/__tests__/ProjectItem.test.js
+++ src/__tests__/ProjectItem.test.js
@@ -1,40 +1,43 @@
+// src/__tests__/ProjectList.test.js
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import ProjectItem from "../components/ProjectItem";
+import ProjectList from "../components/ProjectList";
 
-const project = {
-  id: 1,
-  name: "Reciplease",
-  about: "A recipe tracking app",
-  technologies: ["Rails", "Bootstrap CSS"],
-};
+const projects = [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    technologies: ["Rails", "Bootstrap CSS"],
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    technologies: ["React", "Redux"],
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    technologies: ["React", "Redux", "Rails"],
+  },
+];
 
-test("each <span> element has a unique key prop", () => {
-  let errorSpy = jest.spyOn(global.console, "error");
-  render(
-    <ProjectItem
-      name={project.name}
-      about={project.about}
-      technologies={project.technologies}
-    />
-  );
+test("gives each <ProjectItem> a key based on the project id", () => {
+  const errorSpy = jest.spyOn(global.console, "error");
+  render(<ProjectList projects={projects} />);
 
   expect(errorSpy).not.toHaveBeenCalled();
-
+  
   errorSpy.mockRestore();
 });
 
-test("renders a <span> for each technology passed in as a prop", () => {
-  render(
-    <ProjectItem
-      name={project.name}
-      about={project.about}
-      technologies={project.technologies}
-    />
-  );
-  for (const technology of project.technologies) {
-    const span = screen.queryByText(technology);
-    expect(span).toBeInTheDocument();
-    expect(span.tagName).toBe("SPAN");
+test("renders a <ProjectItem> for each project passed in as a prop", () => {
+  render(<ProjectList projects={projects} />);
+
+  for (const project of projects) {
+    // Check if the project name is rendered in the document
+    expect(screen.getByText(project.name)).toBeInTheDocument();
   }
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,16 @@
+
+
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
 import user from "../data/user";
 
-function App() {
+function Links() {
   return (
     <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <h3>Links</h3>
+      <a href="">{user.links.github}</a>
+      <a href="">{user.links.linkedin}</a>
     </div>
   );
 }
 
-export default App;
+export default Links;
